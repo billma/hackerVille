b3deca4e6e131a91d8b08d093fcb429a0d2a7962
bill ma <billma@bills-MacBook-Pro.local>
2013-04-24 23:56:40 -0700

    inite hackerville

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackerville::Application.config.secret_token = 'bfee375cd64f6aaf8455603b57f4dd9d799e78ae1f1219ee03c9fc2bc92b13972d626b4891654305408b48156970a1670657457cc9e1999d43521e01fea12b11'
